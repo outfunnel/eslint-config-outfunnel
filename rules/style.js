66e867fe7ae0fd14e8b3d3b28ddccab98b1545ed
Kert Pjatkin <2316209+kertpjatkin@users.noreply.github.com>
2020-09-21 16:49:45 +0300

    Ignore max-len for comments (#29)
    * Ignore max-len for comments
    * Update packages

--- rules/style.js
+++ rules/style.js
@@ -329,7 +329,7 @@ module.exports = {
 			2,
 			{
 				ignoreUrls: true,
-				ignoreComments: false,
+				ignoreComments: true,
 				ignoreRegExpLiterals: true,
 				ignoreStrings: true,
 				ignoreTemplateLiterals: true
